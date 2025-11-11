f494462dc68e93c35b5d534d5ab8348c0f5abb7e
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-11-11 10:59:26 +0530

    add new chnages

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AvEPeMWPiM6uMMNfvfKn8XFhX5n5BOBSr1LJ6HzYKNE=\"\n}"
---
+++ app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+console.log("GOOGLE_CLIENT_ID:", typeof process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_ID)
+console.log("GOOGLE_CLIENT_SECRET:", typeof process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_CLIENT_SECRET)
+
+export default function Home() {
+    return (
+        <main>
+            HIiiiiiii
+        </main>
+    );
+}
